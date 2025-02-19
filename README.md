f39af7dbac032c6531c84b088ccf95b68c3d3d7a
Alex P <nippy@windowslive.com>
2025-02-19 18:07:09 +0000

    updated docker compose, added dox

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rpi-mediaserver
+1. Create .env file and add necessary values like vpn token, example of file contents in [env.md](env.md)
--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean
+
+#install docker
 curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
